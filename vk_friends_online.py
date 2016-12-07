be117574ea90e28088be816f0b7dd2dad37f7c7f
eremeevfd <dartfedor@mail.ru>
2016-12-07 11:57:24 +0300

    Direct authorization, gettin online friends using your VK acc

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,43 @@
 import vk
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5761423  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    return input()
 
 
 def get_user_password():
-    pass
+    return input()
 
 
 def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+    try:
+        session = vk.AuthSession(
+            app_id=APP_ID,
+            user_login=login,
+            user_password=password,
+            scope='friends'
+        )
+        api = vk.API(session)
+        return api.friends.get(fields='name')
+    except vk.exceptions.VkAuthError:
+        print("Incorrect Login and/or Password")
+        exit()
 
 
 def output_friends_to_console(friends_online):
-    pass
+    for friend in friends_online:
+        if friend.get('online'):
+            print(friend.get('first_name'), friend.get('last_name'))
+
 
 if __name__ == '__main__':
+    print("Enter your login: ")
     login = get_user_login()
+    print("Enter your password: ")
     password = get_user_password()
     friends_online = get_online_friends(login, password)
+    print("List of your online friends: ")
     output_friends_to_console(friends_online)
