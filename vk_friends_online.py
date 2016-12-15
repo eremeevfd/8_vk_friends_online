008f68facd0064109d256e23cf0d277b3d4e9e16
eremeevfd <dartfedor@mail.ru>
2016-12-15 07:55:47 +0300

    fixed get_online_friends to return just online friends and output to console using users.get method; getting password is now hidden

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,4 +1,5 @@
 import vk
+import getpass
 
 
 APP_ID = 5761423
@@ -9,7 +10,7 @@ def get_user_login():
 
 
 def get_user_password():
-    return input("Enter your password: ")
+    return getpass.getpass(prompt="Enter your password: ")
 
 
 def get_online_friends(login, password):
@@ -21,16 +22,19 @@ def get_online_friends(login, password):
             scope='friends'
         )
         api = vk.API(session)
-        friends = api.friends.get(fields='online')
-        return [friend for friend in friends if friend['online']==1]
+        online_friends_ids = api.friends.getOnline()
+        return online_friends_ids
     except vk.exceptions.VkAuthError:
         print("Incorrect Login and/or Password")
         exit()
 
 
 def output_friends_to_console(friends_online):
+    session = vk.Session()
+    api = vk.API(session)
     for friend in friends_online:
-        print(friend['first_name'], friend['last_name'])
+        friend_info = api.users.get(user_ids=friend)
+        print(friend_info[0]['first_name'], friend_info[0]['last_name'])
 
 
 if __name__ == '__main__':
